588a391fd0bf22c035d02cb122f9db9df447e819
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-03-10 19:06:33 +0800

    feature: create skeleton for explore scene

--- LottieFiles/Scenes/Explore/ExploreViewController.swift
+++ LottieFiles/Scenes/Explore/ExploreViewController.swift
@@ -6,9 +6,23 @@
 //
 
 import UIKit
+import LZViewPager
 
 class ExploreViewController: UIViewController {
 
+    enum Pages: Int, CaseIterable {
+        case recent
+        case featured
+        case popular
+    }
+
+    lazy var viewPager: LZViewPager = {
+        let view = LZViewPager()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    
     private let viewModel: ExploreViewModel
     
     init(viewModel: ExploreViewModel = ExploreViewModel()) {
@@ -22,5 +36,61 @@ class ExploreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        viewPager.dataSource = self
+        viewPager.delegate = self
+        viewPager.hostController = self
+        viewPager.reload()
+    }
+}
+
+extension ExploreViewController {
+    private func setupUI() {
+        view.addSubview(viewPager)
+
+        NSLayoutConstraint.activate([
+            viewPager.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            viewPager.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            viewPager.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            viewPager.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ExploreViewController: LZViewPagerDelegate {}
+
+extension ExploreViewController: LZViewPagerDataSource {
+    func numberOfItems() -> Int {
+        return Pages.allCases.count
+    }
+    
+    func controller(at index: Int) -> UIViewController {
+        guard let page = Pages(rawValue: index) else { return UIViewController() }
+        switch page {
+        case .recent:
+            return ExploreListViewController()
+        case .featured:
+            return ExploreListViewController()
+        case .popular:
+            return ExploreListViewController()
+        }
+    }
+    
+    func button(at index: Int) -> UIButton {
+        guard let page = Pages(rawValue: index) else { return UIButton() }
+        let button = UIButton()
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        
+        switch page {
+        case .recent:
+            button.setTitle("Recent", for: .normal)
+        case .featured:
+            button.setTitle("Featured", for: .normal)
+        case .popular:
+            button.setTitle("Popular", for: .normal)
+        }
+
+        return button
     }
 }
