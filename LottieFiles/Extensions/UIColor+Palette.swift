2d0eb41a39e37bf9674bca7f93ce21d72b837f77
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-03-14 16:58:35 +0800

    feature: add custom colors

---
+++ LottieFiles/Extensions/UIColor+Palette.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Palette.swift
+//  LottieFiles
+//
+//  Created by AD0502-ADE-MB-1 on 14/03/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    enum App {
+        static let primary = UIColor(named: "Color.Primary")
+        static let tabBarFill = UIColor(named: "Color.TabBar.Fill")
+        static let tabBarDefault = UIColor(named: "Color.TabBar.Default")
+    }
+}
