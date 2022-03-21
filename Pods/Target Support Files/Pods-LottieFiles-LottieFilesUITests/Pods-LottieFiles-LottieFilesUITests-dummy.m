2063c39cbc5ae3038d2085941abb732dff13796f
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-03-21 21:37:39 +0800

    chore: add pods

---
+++ Pods/Target Support Files/Pods-LottieFiles-LottieFilesUITests/Pods-LottieFiles-LottieFilesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LottieFiles_LottieFilesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LottieFiles_LottieFilesUITests
+@end
---
+++ Pods/Target Support Files/Pods-LottieFilesTests/Pods-LottieFilesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LottieFilesTests : NSObject
+@end
+@implementation PodsDummy_Pods_LottieFilesTests
+@end
